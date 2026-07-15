a8d777e6f45822ad0188b358801fe23df39a3103
curtiswallace-sys <296265820+curtiswallace-sys@users.noreply.github.com>
2026-07-15 12:57:55 -0400

    Improve formatting and print statements in QuikMath2.py
    Refactor print statements and formatting in QuikMath2.py

--- QuikMath2.py
+++ QuikMath2.py
@@ -1,13 +1,17 @@
-def nba(): 
-    print("Swiss Army Knife")
+def nba(): print("Swiss Army Knife")
+print("Build:") 
 nba()
 
 def add(a, b):
     return a + b
 result = add(6, 5)
+print(" ")
+print("Add:") 
 print(result)
 
 def averages(numbers):
     return sum(numbers) / len(numbers)
 result = averages([33, 11, 20,])
+print(" ")
+print("Average:")
 print(result)
