cf104f68b7621d2068a679471c14f09652d6e314
curtiswallace-sys <296265820+curtiswallace-sys@users.noreply.github.com>
2026-07-15 12:10:06 -0400

    Add files via upload

---
+++ QuikMath2.py
@@ -0,0 +1,13 @@
+def nba(): 
+    print("Swiss Army Knife")
+nba()
+
+def add(a, b):
+    return a + b
+result = add(6, 5)
+print(result)
+
+def averages(numbers):
+    return sum(numbers) / len(numbers)
+result = averages([33, 11, 20,])
+print(result)
