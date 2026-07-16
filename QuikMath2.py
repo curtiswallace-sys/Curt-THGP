ce60f31e5fed766180ec24d7b71260d55f232122
curtiswallace-sys <296265820+curtiswallace-sys@users.noreply.github.com>
2026-07-16 11:53:18 -0400

    Add minus and divide functions to QuikMath2.py

--- QuikMath2.py
+++ QuikMath2.py
@@ -9,6 +9,20 @@ print(" ")
 print("Add:") 
 print(result)
 
+def minus(a, b):
+    return a - b
+result = minus(6, 5)
+print(" ")
+print("Minus:")
+print(result)
+
+def divide(a, b):
+    return a / b
+result = divide(6, 5)
+print(" ")
+print("Divide:")
+print(result)
+
 def averages(numbers):
     return sum(numbers) / len(numbers)
 result = averages([33, 11, 20,])
