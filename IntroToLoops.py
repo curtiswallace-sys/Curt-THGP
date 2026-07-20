24dd0b902fada03c83ba518d5744f12cf59fc311
curtiswallace-sys <296265820+curtiswallace-sys@users.noreply.github.com>
2026-07-20 12:25:40 -0400

    Add files via upload

---
+++ IntroToLoops.py
@@ -0,0 +1,15 @@
+thislist = ["Shooting", "Finishing", "Playmaking", "Defence"]
+thatlist = ["Athleticism", "Rebounding"]
+print(thislist)
+print(thatlist)
+
+games = ["cod", "2k", "gta"]
+for x in games:
+  if x == "2k":
+    continue
+  print(x)
+
+  i = -3
+while i < 4:
+  print(i)
+  i += 1
