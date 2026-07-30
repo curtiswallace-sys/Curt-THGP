737e0863e847ee51d27e9dae0f8c964c9ecac93f
curtiswallace-sys <296265820+curtiswallace-sys@users.noreply.github.com>
2026-07-30 14:48:46 -0400

    Add files via upload

---
+++ OOP.py
@@ -0,0 +1,10 @@
+class Music:
+    def __init__(self, sing):
+        self.sing = genre 
+
+def genre(sing):
+
+print("What genre of music do you want to hear?:")
+genre= input()
+print(f"playing{genre}")
+
